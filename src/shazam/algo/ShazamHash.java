309b791c13ddb6935fae3ec71cd9a337d80c5339
DavaiZa <306674698@qq.com>
2016-10-20 12:19:45 +0800

    Hashing

---
+++ src/shazam/algo/ShazamHash.java
@@ -0,0 +1,11 @@
+package shazam.algo;
+
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+public class ShazamHash {
+    public int f1;       // the first frequency peak
+    public int f2;       // the second frequency peak
+    public int dt;       // the time difference between two frequency peaks
+    public int offset;   // the offset to the beginning of music
+}
