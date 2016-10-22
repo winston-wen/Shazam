3a13507e8ff8e2315043e5afe632deb34d254212
DavaiZa <306674698@qq.com>
2016-10-22 09:55:48 +0800

    music archiver

--- src/shazam/MusicSearcher.java
+++ src/shazam/MusicSearcher.java
@@ -1,5 +1,7 @@
 package shazam;
 
+import java.io.File;
+
 /**
  * Created by Administrator on 2016/10/20.
  *
@@ -7,4 +9,20 @@ package shazam;
  * TODO: Compute their fingerprints, match against fingerprints in DB, and find the best match.
  */
 public class MusicSearcher {
+    public static void main(String[] args) {
+        if (args.length!=2) {
+            throw new RuntimeException("usage: java MusicSearcher <input folder>");
+        }
+        File f = new File(args[1]);
+        if (!f.isDirectory()) {
+            throw new RuntimeException(String.format("%s is not a directory", args[1]));
+        }
+        File[] songs = f.listFiles();
+        for (File song : songs) {
+            // only process .wav files
+            if (song.getName().toLowerCase().endsWith(".wav")) {
+
+            }
+        }
+    }
 }
